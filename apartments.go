6a84c787f5cfb24257e0869dfc1e75729ced4645
Vasil <vaskovasilev94@yahoo.com>
2020-06-16 14:44:34 +0200

    Refactored telegram support.

--- apartments.go
+++ apartments.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
 	"github.com/mitchellh/go-homedir"
 	"github.com/sp0x/rented/bots"
 	"github.com/sp0x/rented/sites"
@@ -87,30 +88,38 @@ func findApartments(cmd *cobra.Command, args []string) {
 //Reads the channel that's the result of watching an indexer.
 func readIndexer(resultsChan <-chan search.ExternalResultItem) {
 	chatMessagesChannel := make(chan bots.ChatMessage)
-	telegram := bots.NewTelegram()
-	go telegram.Run()
-	go telegram.FeedBroadcast(chatMessagesChannel)
-	for true {
-		select {
-		case result := <-resultsChan:
-			//log.Infof("New result: %s\n", result)
-			if result.IsNew() || result.IsUpdate() {
-				price := result.GetField("price")
-				reserved := result.GetField("reserved")
-				if reserved == "true" {
-					reserved = "It's currently reserved"
-				} else {
-					reserved = "And not reserved yet!!!"
-				}
-				msgText := fmt.Sprintf("I found a new property\n"+
-					"[%s](%s)\n"+
-					"*%s* - %s", result.Title, result.Link, price, reserved)
-				message := bots.ChatMessage{Text: msgText, Banner: result.Banner}
-				chatMessagesChannel <- message
-				area := result.Size
-				fmt.Printf("[%s][%d][%s] %s - %s\n", price, area, reserved, result.ResultItem.Title, result.Link)
+	token := viper.GetString("TELEGRAM_TOKEN")
+	telegram, err := bots.NewTelegram(token, tgbotapi.NewBotAPI)
+	if err != nil {
+		fmt.Printf("Couldn't initialize telegram bot: %v", err)
+		os.Exit(1)
+	}
+	go func() {
+		_ = telegram.Run()
+	}()
+	go func() {
+		_ = telegram.FeedBroadcast(chatMessagesChannel)
+	}()
+	for {
+		result := <-resultsChan
+		//log.Infof("New result: %s\n", result)
+		if result.IsNew() || result.IsUpdate() {
+			price := result.GetField("price")
+			reserved := result.GetField("reserved")
+			if reserved == "true" {
+				reserved = "It's currently reserved"
+			} else {
+				reserved = "And not reserved yet!!!"
 			}
+			msgText := fmt.Sprintf("I found a new property\n"+
+				"[%s](%s)\n"+
+				"*%s* - %s", result.Title, result.Link, price, reserved)
+			message := bots.ChatMessage{Text: msgText, Banner: result.Banner}
+			chatMessagesChannel <- message
+			area := result.Size
+			fmt.Printf("[%s][%d][%s] %s - %s\n", price, area, reserved, result.ResultItem.Title, result.Link)
 		}
+
 	}
 
 	//We store them here also, so we have faster access
--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.12
 require (
 	github.com/go-telegram-bot-api/telegram-bot-api v4.6.4+incompatible
 	github.com/mitchellh/go-homedir v1.1.0
+	github.com/onsi/gomega v1.7.1
 	github.com/sirupsen/logrus v1.6.0
 	github.com/sp0x/torrentd v0.0.0-20200607060337-bcc478604370
 	github.com/spf13/cobra v1.0.0
